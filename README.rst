7fe33fc68d0ff77828a80b4d70f375ed69edba9e
Romain Dorgueil <romain@dorgueil.net>
2014-01-13 17:01:14 +0100

    initial commit

---
+++ README.rst
@@ -0,0 +1,44 @@
+==========
+rdc.common
+==========
+
+``rdc.common`` contains a few simple general purpose utilities that are used by various different projects.
+
+.. image:: https://secure.travis-ci.org/rdcli/common.png
+  :target: http://travis-ci.org/rdcli/common
+  :alt: Build Status
+
+
+Documentation
+-------------
+
+`Full documentation for rdc.etl on ReadTheDocs <http://common.rdc.li/>`_.
+
+
+Running the Test Suite
+----------------------
+
+.. code-block:: shell
+
+    pip install nose
+    make test
+
+
+Release Notes
+-------------
+
+
+Contributing
+------------
+
+I'm Romain Dorgueil.
+
+``rdc.etl`` is on `GitHub <https://github.com/rdcli/>`_.
+
+Get in touch, via GitHub or otherwise, if you've got something to contribute,
+it'd be most welcome!
+
+If you feel overwhelmingly grateful, or want to support the project you can tip
+me on `Gittip <https://www.gittip.com/rdorgueil/>`_.
+
+
---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+from rdc.common import __version__
+
+# Human version
+version = '.'.join(map(str, __version__))
+
+# Read README file
+with open('README.rst') as readme:
+    long_description = readme.read()
+
+# Parse classifiers
+with open('classifiers.txt') as f:
+    classifiers = filter(None, map(lambda s: s.strip(), f.read().split('\n')))
+
+# Parse requirements
+with open('requirements.txt') as f:
+    install_requires = []
+    for requirement in map(lambda s: s.strip(), f.read().split('\n')):
+        _pound_pos = requirement.find('#')
+        if _pound_pos != -1:
+            requirement = requirement[0:_pound_pos].strip()
+        if len(requirement):
+            install_requires.append(requirement)
+
+setup(
+    name='rdc.common',
+    version=version,
+    description="Common tools",
+    long_description=long_description,
+    classifiers=classifiers,
+    author='Romain Dorgueil',
+    author_email='romain@dorgueil.net',
+    url='http://common.rdc.li/',
+    download_url='https://github.com/rdcli/rdc.common/tarball/' + version,
+    license='Apache License, Version 2.0',
+    packages=find_packages(exclude=['ez_setup', 'example', 'test']),
+    install_requires=install_requires,
+)
