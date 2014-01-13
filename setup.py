1abf8df2418f298942eb0aa15d643affa2e940e1
Romain Dorgueil <romain@dorgueil.net>
2014-01-13 18:40:47 +0100

    again an attempt to fix this setup.py to be useable in intra namespace requirements specs

--- setup.py
+++ setup.py
@@ -1,24 +1,31 @@
 from setuptools import setup, find_packages
 
-with open('VERSION') as f: version = f.read()
-with open('README.rst') as f: long_description = f.read()
-with open('classifiers.txt') as f: classifiers = filter(None, map(lambda s: s.strip(), f.read().split('\n')))
-with open('requirements.txt') as f:
+tolines = lambda c: filter(None, map(lambda s: s.strip(), c.split('\n')))
+
+def read(filename, flt=None):
+    with open(filename) as f:
+        content = f.read().strip()
+        return flt(content) if callable(flt) else content
+
+def requirements_filter(c):
     install_requires = []
-    for requirement in map(lambda s: s.strip(), f.read().split('\n')):
+    for requirement in tolines(c):
         _pound_pos = requirement.find('#')
         if _pound_pos != -1:
             requirement = requirement[0:_pound_pos].strip()
         if len(requirement):
             install_requires.append(requirement)
+    return install_requires
+
+version = read('version.txt')
 
 setup(
     name='rdc.common',
     namespace_packages = ['rdc'],
     version=version,
     description="Common tools",
-    long_description=long_description,
-    classifiers=classifiers,
+    long_description=read('README.rst'),
+    classifiers=read('classifiers.txt', tolines),
     author='Romain Dorgueil',
     author_email='romain@dorgueil.net',
     url='http://common.rdc.li/',
@@ -26,5 +33,5 @@ setup(
     license='Apache License, Version 2.0',
     packages=find_packages(exclude=['ez_setup', 'example', 'test']),
     include_package_data=True,
-    install_requires=install_requires,
+    install_requires=read('requirements.txt', requirements_filter),
 )
